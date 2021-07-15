20074f8a308f140a565ee4b25ea7ead5f994424b
kingachwaleba <kingach6@gmail.com>
2021-07-15 20:18:42 +0200

    Remove unused import statement

--- backend/src/main/java/com/team/backend/model/WalletCategory.java
+++ backend/src/main/java/com/team/backend/model/WalletCategory.java
@@ -6,8 +6,6 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.HashSet;
-import java.util.Set;
 
 @Data
 @NoArgsConstructor
