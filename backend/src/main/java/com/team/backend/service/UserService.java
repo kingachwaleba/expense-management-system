b4ebb1b1dce957a2e617d0fa61aee82fd68fff99
kingachwaleba <kingach6@gmail.com>
2021-07-16 18:45:59 +0200

    Provide service for registering an user

--- backend/src/main/java/com/team/backend/service/UserService.java
+++ backend/src/main/java/com/team/backend/service/UserService.java
@@ -1,4 +1,11 @@
 package com.team.backend.service;
 
-public class UserService {
+import com.team.backend.model.User;
+
+import java.util.Optional;
+
+public interface UserService {
+
+    void save(User user);
+    Optional<User> findByLogin(String login);
 }
--- backend/src/main/java/com/team/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/UserServiceImpl.java
@@ -1,4 +1,31 @@
 package com.team.backend.service;
 
-public class UserServiceImpl {
+import com.team.backend.model.User;
+import com.team.backend.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Override
+    public void save(User user) {
+        user.setPasswordHash(bCryptPasswordEncoder.encode(user.getPasswordHash()));
+        userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByLogin(String login) {
+        return userRepository.findByLogin(login);
+    }
 }
