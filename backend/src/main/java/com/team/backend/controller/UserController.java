48af126d72a3f002b277115bfaca27d5dae8fe1e
kingachwaleba <kingach6@gmail.com>
2021-07-16 20:37:11 +0200

    Create UserController class

--- backend/src/main/java/com/team/backend/controller/UserController.java
+++ backend/src/main/java/com/team/backend/controller/UserController.java
@@ -1,4 +1,31 @@
 package com.team.backend.controller;
 
+import com.team.backend.model.User;
+import com.team.backend.service.SecurityService;
+import com.team.backend.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
 public class UserController {
+
+    private final UserService userService;
+    private final SecurityService securityService;
+
+    public UserController(UserService userService, SecurityService securityService) {
+        this.userService = userService;
+        this.securityService = securityService;
+    }
+
+    @PostMapping("/registration")
+    public ResponseEntity<String> createAccount(@Valid @RequestBody User user) {
+        userService.save(user);
+        securityService.autoLogin(user.getLogin(), user.getPassword());
+
+        return ResponseEntity.ok("User has been created");
+    }
 }
