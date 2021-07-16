00815a7b43abc72cfa207c9e3b40964b3d3cf316
kingachwaleba <kingach6@gmail.com>
2021-07-16 22:48:01 +0200

    Disable CSRF protection

--- backend/src/main/java/com/team/backend/config/WebSecurityConfig.java
+++ backend/src/main/java/com/team/backend/config/WebSecurityConfig.java
@@ -39,6 +39,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
         http
                 .httpBasic()
                 .and()
