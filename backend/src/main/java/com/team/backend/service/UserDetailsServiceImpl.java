aa2ce8c2800fe541478e82213eb7d6588937bc3a
kingachwaleba <kingach6@gmail.com>
2021-07-16 18:22:27 +0200

    Implement UserDetailsService interface

--- backend/src/main/java/com/team/backend/service/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/UserDetailsServiceImpl.java
@@ -1,4 +1,32 @@
 package com.team.backend.service;
 
-public class UserDetailsServiceImpl {
+import com.team.backend.model.User;
+import com.team.backend.repository.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByLogin(s);
+        if (optionalUser.isEmpty()) throw new UsernameNotFoundException(s);
+        User user = optionalUser.get();
+
+        return new org.springframework.security.core.userdetails.User(user.getLogin(), user.getPasswordHash(), Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
 }
