f8b236f08ed261dd67c4965f4234e12ced17fec4
kingachwaleba <kingach6@gmail.com>
2021-07-17 17:36:03 +0200

    Set deleted flag during register

--- backend/src/main/java/com/team/backend/service/UserServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/UserServiceImpl.java
@@ -21,6 +21,7 @@ public class UserServiceImpl implements UserService {
     @Override
     public void save(User user) {
         user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        user.setDeleted("N");
         userRepository.save(user);
     }
 
