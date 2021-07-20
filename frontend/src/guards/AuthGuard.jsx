97c8d7841010ed07c517d0013b8037709bd46c39
kingachwaleba <kingach6@gmail.com>
2021-07-20 12:09:17 +0200

    Provide authentication for routes

---
+++ frontend/src/guards/AuthGuard.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Redirect} from "react-router-dom";
+import UserService from "../services/user.service";
+
+class AuthGuard extends React.Component {
+
+
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        const {component: Component} = this.props;
+        const currentUser = UserService.currentUserValue;
+
+        if (!currentUser) {
+            return (<Redirect to={{pathname: '/login'}}/>)
+        }
+
+        return (<Component/>);
+    }
+}
