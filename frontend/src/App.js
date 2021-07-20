7cb5159e0c5710b713f0c9b7491a3a589e515a06
kingachwaleba <kingach6@gmail.com>
2021-07-20 12:20:47 +0200

    Create a navigation bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,23 +6,110 @@ import {LoginPage} from "./pages/login/LoginPage";
 import {RegisterPage} from "./pages/register/RegisterPage";
 import {HomePage} from "./pages/home/HomePage";
 import AuthGuard from './guards/AuthGuard';
+import UserService from "./services/user.service";
 
 class App extends React.Component {
-
 
     constructor(props, context) {
         super(props, context);
 
         this.state = {
             history: createBrowserHistory(),
+            currentUser: null,
         };
     }
 
+    componentDidMount() {
+        UserService.currentUser.subscribe(data => {
+            this.setState({
+                currentUser: data,
+            });
+        });
+    }
+
+    logout() {
+        UserService.logOut()
+            .then(
+                data => {
+                    this.state.push('/login');
+                },
+                error => {
+                    this.setState({
+                        errorMessage: 'Unexpected error occurred.',
+                    });
+                },
+            );
+    }
+
     render() {
-        const {history} = this.state;
+        const {currentUser, history} = this.state;
 
         return (
             <Router history={history}>
+                <div>
+                    {currentUser &&
+                    <nav className="navbar navbar-expand navbar-dark bg-dark">
+                        <a className="navbar-brand" href="https://reactjs.org">
+                            React
+                        </a>
+                        <div className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/home">
+                                    <span className="fa fa-home"/>
+                                    Home
+                                </a>
+                            </li>
+                        </div>
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/profile">
+                                    <span className="fa fa-user"/>
+                                    {currentUser.name}
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" onClick={() => this.logout()}>
+                                    <span className="fa fa-sign-out"/>
+                                    LogOut
+                                </a>
+                            </li>
+                        </div>
+                    </nav>
+                    }
+                </div>
+
+                <div>
+                    {!currentUser &&
+                    <nav className="navbar navbar-expand navbar-dark bg-dark">
+                        <a className="navbar-brand" href="https://reactjs.org">
+                            React
+                        </a>
+                        <div className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/home">
+                                    <span className="fa fa-home"/>
+                                    Home
+                                </a>
+                            </li>
+                        </div>
+                        <div className="navbar-nav ml-auto">
+                            <li className="nav-item">
+                                <a className="nav-link" href="/register">
+                                    <span className="fa fa-user-plus"/>
+                                    &nbsp;
+                                    Register
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/login">
+                                    <span className="fa fa-sign-in"/>
+                                    Login
+                                </a>
+                            </li>
+                        </div>
+                    </nav>
+                    }
+                </div>
                 <div className="container">
                     <Switch>
                         <Route exact path="/login" component={LoginPage}/>
