69c86c3bcd4c807af697311aa215eead4d494d89
kingachwaleba <kingach6@gmail.com>
2021-07-20 11:22:58 +0200

    Initial commit

---
+++ frontend/src/models/user.js
@@ -0,0 +1,10 @@
+export class User {
+
+    constructor(id, login, email, password, image) {
+        this.id = id;
+        this.login = login;
+        this.email = email;
+        this.password = password;
+        this.image = image;
+    }
+}
