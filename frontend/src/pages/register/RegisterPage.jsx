4ead14a34b9bedd6f27207deb2568131238b81f5
kingachwaleba <kingach6@gmail.com>
2021-07-20 10:23:22 +0200

    Initial commit

---
+++ frontend/src/pages/register/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class RegisterPage extends React.Component {
+
+    constructor(props, context) {
+        super(props, context);
+    }
+}
+
+export { RegisterPage };
