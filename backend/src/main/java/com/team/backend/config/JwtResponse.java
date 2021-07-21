acfce72d56a607a883058ec03c953818deb954a9
kingachwaleba <kingach6@gmail.com>
2021-07-21 09:54:04 +0200

    Create jwt response returned after successful authentication

--- backend/src/main/java/com/team/backend/config/JwtResponse.java
+++ backend/src/main/java/com/team/backend/config/JwtResponse.java
@@ -1,4 +1,23 @@
 package com.team.backend.config;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+@Getter
+@Setter
 public class JwtResponse {
+
+    private String token;
+    private String type = "Bearer";
+    private String login;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public JwtResponse(String accessToken, String login, Collection<? extends GrantedAuthority> authorities) {
+        this.token = accessToken;
+        this.login = login;
+        this.authorities = authorities;
+    }
 }
