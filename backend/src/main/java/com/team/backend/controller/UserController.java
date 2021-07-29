002a9e0c4dc9c72a455313a0649f8ebd1a257c17
kingachwaleba <kingach6@gmail.com>
2021-07-29 19:20:13 +0200

    Return data about current logged in user

--- backend/src/main/java/com/team/backend/controller/UserController.java
+++ backend/src/main/java/com/team/backend/controller/UserController.java
@@ -37,6 +37,16 @@ public class UserController {
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
+    @GetMapping("/account")
+    public ResponseEntity<?> one() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUserLogin = authentication.getName();
+
+        User user = userService.findByLogin(currentUserLogin).orElseThrow(RuntimeException::new);
+
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest) {
         Authentication authentication = authenticationManager.authenticate(
