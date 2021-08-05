f503909336cbcbca900d727bc5de6ac299a3bab8
kingachwaleba <kingach6@gmail.com>
2021-08-05 16:47:31 +0200

    Initial commit

---
+++ backend/src/main/java/com/team/backend/repository/MessageRepository.java
@@ -0,0 +1,7 @@
+package com.team.backend.repository;
+
+import com.team.backend.model.Message;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MessageRepository extends JpaRepository<Message, Integer> {
+}
