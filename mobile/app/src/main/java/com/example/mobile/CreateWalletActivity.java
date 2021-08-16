d1dcfd9a831981b40fb013ed6fe0c4da44fed1d8
Hakijuu <chwil.malgorzata99@gmail.com>
2021-08-16 15:09:42 +0200

    Correct create_wallet layout

--- mobile/app/src/main/java/com/example/mobile/CreateWalletActivity.java
+++ mobile/app/src/main/java/com/example/mobile/CreateWalletActivity.java
@@ -10,5 +10,9 @@ public class CreateWalletActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_wallet);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setLogo(R.drawable.pagename);
+        getSupportActionBar().setDisplayUseLogoEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 }
