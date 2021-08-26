ac356da436e79816069f9281e15e4cd52b74382d
kingachwaleba <kingach6@gmail.com>
2021-08-26 13:46:32 +0200

    Change PUTmethod that edits a wallet

--- backend/src/main/java/com/team/backend/repository/WalletCategoryRepository.java
+++ backend/src/main/java/com/team/backend/repository/WalletCategoryRepository.java
@@ -3,5 +3,9 @@ package com.team.backend.repository;
 import com.team.backend.model.WalletCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WalletCategoryRepository extends JpaRepository<WalletCategory, Integer> {
+
+    Optional<WalletCategory> findByName(String name);
 }
