18a9ebdf950f528141a13f8e546525b5c0460e30
Hakijuu <chwil.malgorzata99@gmail.com>
2021-10-03 17:33:47 +0200

    Init change image function

--- mobile/app/src/main/java/com/example/mobile/activity/EditProfileActivity.java
+++ mobile/app/src/main/java/com/example/mobile/activity/EditProfileActivity.java
@@ -1,24 +1,131 @@
 package com.example.mobile.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.loader.content.CursorLoader;
+
 import com.example.mobile.R;
+import com.example.mobile.config.ApiClient;
+import com.example.mobile.config.ApiInterface;
+import com.example.mobile.config.SessionManager;
+
+import java.io.File;
+
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class EditProfileActivity extends BaseActivity {
 
-    Button changePasswordBtn;
+    Button changePasswordBtn, chooseImageBtn, saveChangeBtn, deleteAccountBtn;
+    SessionManager session;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
 
+        chooseImageBtn = findViewById(R.id.choose_image_btn);
+        saveChangeBtn = findViewById(R.id.save_change_btn);
         changePasswordBtn = findViewById(R.id.change_password_btn);
+        deleteAccountBtn = findViewById(R.id.delete_account_btn);
+
+        session = new SessionManager(this);
+
+        chooseImageBtn.setOnClickListener(v -> {
+            Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(i, 100);
+        });
 
         changePasswordBtn.setOnClickListener(v -> {
             Intent intent = new Intent(EditProfileActivity.this, ChangePasswordActivity.class);
             startActivity(intent);
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                    Uri.parse("package:" + getPackageName()));
+            finish();
+            startActivity(intent);
+            return;
+        }
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK && data != null) {
+            //the image URI
+            Uri selectedImage = data.getData();
+
+            uploadFile(selectedImage, "users");
+        }
+    }
+
+    private String getRealPathFromURI(Uri contentUri) {
+        String[] proj = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(this, contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+    private void uploadFile(Uri fileUri, String desc) {
+
+        //creating a file
+        File file = new File(getRealPathFromURI(fileUri));
+
+        //creating request body for file
+        RequestBody requestFile = RequestBody.create(MediaType.parse("multipart/form-data"), file);
+        MultipartBody.Part body = MultipartBody.Part.createFormData("image", file.getName(), requestFile);
+
+        String directory = "users";
+       // RequestBody descBody = RequestBody.create(MediaType.parse("text/plain"), directory);
+
+        ApiInterface apiInterface = new ApiClient().getService();
+
+        System.out.println(session.getUserDetails().get(SessionManager.KEY_TOKEN));
+
+        //creating a call and calling the upload image method
+        Call<ResponseBody> call = apiInterface.upload(session.getUserDetails().get(SessionManager.KEY_TOKEN), body, directory);
+
+        //finally performing the call
+        call.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                System.out.println(response.code());
+            }
+
+            @Override
+            public void onFailure(Call<ResponseBody> call, Throwable t) {
+                System.out.println(call.toString());
+                System.out.println(t.toString());
+                Toast.makeText(getApplicationContext(), "Coś poszło nie tak" , Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
 }
