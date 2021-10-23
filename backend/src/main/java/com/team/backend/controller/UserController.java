1381115754165fe5b87638665bd84a4169241d05
kingachwaleba <kingach6@gmail.com>
2021-10-23 20:21:11 +0200

    Enable users to delete their profile pics

--- backend/src/main/java/com/team/backend/controller/UserController.java
+++ backend/src/main/java/com/team/backend/controller/UserController.java
@@ -196,6 +196,15 @@ public class UserController {
         }
     }
 
+    @PutMapping("/account/delete-profile-picture")
+    public ResponseEntity<?> deleteImage() {
+        User user = userService.findCurrentLoggedInUser().orElseThrow(UserNotFoundException::new);
+        user.setImage(null);
+        userService.save(user);
+
+        return new ResponseEntity<>("User image has been deleted!", HttpStatus.OK);
+    }
+
     @PutMapping("/delete-account")
     public ResponseEntity<?> deleteAccount(@RequestBody TextNode password) {
         User user = userService.findCurrentLoggedInUser().orElseThrow(UserNotFoundException::new);
