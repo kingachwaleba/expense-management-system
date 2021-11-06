ea64fc8153053f9aeb2c832c9874bad02aaa1ddd
kingachwaleba <kingach6@gmail.com>
2021-11-06 15:33:20 +0100

    Create StatusController

---
+++ backend/src/main/java/com/team/backend/controller/StatusController.java
@@ -0,0 +1,27 @@
+package com.team.backend.controller;
+
+import com.team.backend.model.Status;
+import com.team.backend.repository.StatusRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class StatusController {
+
+    private final StatusRepository statusRepository;
+
+    public StatusController(StatusRepository statusRepository) {
+        this.statusRepository = statusRepository;
+    }
+
+    @GetMapping("/categories")
+    public ResponseEntity<?> getAllStatuses() {
+        List<Status> statusList = statusRepository.findAll();
+
+        return new ResponseEntity<>(statusList, HttpStatus.OK);
+    }
+}
