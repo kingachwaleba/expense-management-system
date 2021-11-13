03f190421512827eaf14389d6a23fd9229d7ef80
kingachwaleba <kingach6@gmail.com>
2021-11-13 15:49:36 +0100

    Change error messages

--- backend/src/main/java/com/team/backend/controller/UserController.java
+++ backend/src/main/java/com/team/backend/controller/UserController.java
@@ -72,7 +72,7 @@ public class UserController {
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginForm loginRequest, BindingResult bindingResult) {
         if (userService.getErrorList(bindingResult).size() != 0)
-            return new ResponseEntity<>(errorMessage.get("login.error"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorMessage.get("data.error"), HttpStatus.BAD_REQUEST);
 
         String email = loginRequest.getEmail();
         String password = loginRequest.getPassword();
@@ -103,7 +103,7 @@ public class UserController {
         String confirmPassword = registrationForm.getConfirmPassword();
 
         if (userService.validation(bindingResult, user.getPassword()).size() != 0)
-            return new ResponseEntity<>(errorMessage.get("user.password.error"), HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(errorMessage.get("data.error"), HttpStatus.BAD_REQUEST);
 
         if (!userService.checkIfValidConfirmPassword(user.getPassword(), confirmPassword))
             return new ResponseEntity<>(errorMessage.get("register.confirmPassword"), HttpStatus.BAD_REQUEST);
